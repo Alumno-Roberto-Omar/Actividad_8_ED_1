e4c7c61e08a0ad01a031eecdc4335edef42c5434
“Alumno-Roberto-Omar” <roberto.perez2891@alumnos.udg.mx>
2020-10-21 15:17:23 -0500

    trabajamos con los tres metodos, primero con los de respaldo y luego con los de recuperacion

--- main.cpp
+++ main.cpp
@@ -5,21 +5,31 @@ using namespace std;
 
 int main()
 {
-    Computadora c1 = Computadora("Equipo_nuevo_1","Windows 7",450,4);
-    Computadora c2 = Computadora("PC_Omar","Windows 10", 1000, 8);
-    Computadora c3 = Computadora("PC_User_1","Windows 8",500,4);
-
     Laboratorio lb;
 
-    lb<<c1<<c2<<c3;
+    /*1. Primero se ejecuta esta parte para ingresar los datos y respaldar en un
+    documento de texto. */
+    /*cout<<"Ingrese los datos para el equipo:"<<endl;
+    Computadora c1;
+    cin>>c1;
+    lb<<c1;
 
-    Computadora c4;
-    cin>>c4;
-    lb<<c4;
+    Computadora c2 = Computadora("Equipo_2","Windows 8",600,3);
+    lb<<c2;
 
+    Computadora c3= Computadora("Equipo_3","Windows 10",1000,4);
+    lb<<c3;*/
+
+    //lb.respaldartabla();
+    //lb.respaldar();
+
+    /*2. Esta parte del código sirve para recuperar los datos del documento de 
+    texto y mostrarlos, la ejecuto aparte porque al ejecutarla junto con el 
+    método para respaldar, pues el método de recuperacion leera dos veces el 
+    documento de texto. */
     cout<<endl;
-
+    lb.recuperar();
     lb.mostrar();
-    
+
     return 0;
 }
