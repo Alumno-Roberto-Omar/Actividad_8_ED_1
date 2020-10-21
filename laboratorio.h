29004a21bb16ce89344a85334d0bf817639a521d
“Alumno-Roberto-Omar” <roberto.perez2891@alumnos.udg.mx>
2020-10-21 15:16:23 -0500

    Agregamos tres nuevos metodos: respaldartabla, respaldar y recuperar.

--- laboratorio.h
+++ laboratorio.h
@@ -12,6 +12,9 @@ public:
     Laboratorio();
     void agregarFinal(const Computadora &p);
     void mostrar();
+    void respaldartabla();
+    void respaldar();
+    void recuperar();
 
     friend Laboratorio& operator<<(Laboratorio &v, const Computadora &p)
     {
